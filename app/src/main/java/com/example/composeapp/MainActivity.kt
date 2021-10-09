093a711e83f17f8633352923fec8bede60d3fb0c
Vihaan <vihaan@sharechat.co>
2021-10-09 17:05:50 +0530

    path 1 complete

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.composeapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,52 +32,70 @@ class MainActivity : ComponentActivity() {
             ComposeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MessageCard(Message("Android", "Jetpack Compose"))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeAppTheme {
-        Greeting("Android")
-    }
-}
-
 data class Message(val author: String, val body: String)
-
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row (modifier=Modifier.padding(8.dp)){
+    Row(modifier = Modifier.padding(8.dp)) {
         Image(
             modifier = Modifier
                 .size(40.dp)
-                .clip(CircleShape),
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape),
             painter = painterResource(id = R.drawable.img),
             contentDescription = null)
-        Spacer(modifier=Modifier.width(8.dp))
-        Column {
-            Text(text = msg.author)
-            Spacer(modifier=Modifier.height(4.dp))
-            Text(text = msg.body)
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(text = msg.author, color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2)
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp),
+                elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Converstaion(messages: List<Message>) {
+    LazyColumn {
+        items(messages)
+        { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview(name = "Light mode")
+@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun PreviewMessageCard(
 ) {
-    val msg = Message("Vihaan", "This is jetpack compose")
-    MessageCard(msg)
+    ComposeAppTheme {
+//        Converstaion(messages = SampleData.conversationSample.subList(0, 1))
+        Converstaion(messages = SampleData.conversationSample)
+    }
 }
