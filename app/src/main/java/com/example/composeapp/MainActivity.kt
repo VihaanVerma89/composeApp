f92bb7e96f54d44722a72e88b07c1856e8557757
Vihaan <vihaan@sharechat.co>
2021-10-09 13:13:51 +0530

    configure your layout

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,4 +42,34 @@ fun DefaultPreview() {
     ComposeAppTheme {
         Greeting("Android")
     }
+}
+
+data class Message(val author: String, val body: String)
+
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row (modifier=Modifier.padding(8.dp)){
+        Image(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape),
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = null)
+        Spacer(modifier=Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier=Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMessageCard(
+) {
+    val msg = Message("Vihaan", "This is jetpack compose")
+    MessageCard(msg)
 }
